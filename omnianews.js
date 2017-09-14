44bc235bbf6f833aa4fb97d2c65b1b16ad675d49
Tuan Nguyen <tuannla07@gmail.com>
2017-09-14 18:31:24 +0700

    commit

--- omnianews.js
+++ omnianews.js
@@ -93,14 +93,14 @@ function getNewsItems(session, items){
     var arrNewsItems = [];
     for(var i = 0 ; i < items.length ; i++)
     {
-        arrNewsItems.push(toCard(session, items[i]));
+        arrNewsItems.push(toCard(session, items[i], i));
     }
     return arrNewsItems;
 }
 
-function toCard(session, news) {
+function toCard(session, news, idx) {
     //var imageUrl = process.env.OMNIA_SITE_COLLECTION_URL + $(news.imageHtml).attr("src");
-    var imageUrl = getImage(news);
+    var imageUrl = getImage(news, idx);
     var pageUrl = process.env.OMNIA_SITE_COLLECTION_URL + news.url;
     var dateStr=news.modified.replace(/T|\:\d\dZ/g,' ');
 
@@ -112,13 +112,13 @@ function toCard(session, news) {
         .buttons([builder.CardAction.openUrl(session, pageUrl, 'Read more')]);
 }
 
-function getImage(news){
+function getImage(news ,idx){
     var images =[
-        'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/c4875c3cd4c554ab1e9519a172575044/demo4.jpg',
-        'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/44185827e6d79813135071719c17b5f5/demo3.jpg',
+        'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/017a1ba1525165aa455fd9b8526fef5f/demo1.jpg',
         'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/83461f6e50a1a57def690c6884745ce4/demo2.jpg',
-        'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/017a1ba1525165aa455fd9b8526fef5f/demo1.jpg'
+        'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/44185827e6d79813135071719c17b5f5/demo3.jpg',
+        'https://trello-attachments.s3.amazonaws.com/55123b69640dfffcaabc228e/59ba668b00bdc3af90f9a1a5/c4875c3cd4c554ab1e9519a172575044/demo4.jpg'  
     ];
-    var item = images[Math.floor(Math.random()*images.length)]; 
+    var item = images[idx]; 
     return item;
 }
